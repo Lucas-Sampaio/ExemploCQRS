fa8c7fe4116abb25a4fbf8140c41a84c168b2a64
Lucas Sampaio Souza <lucasssouza29@hotmail.com>
2021-11-04 22:44:23 -0300

    Update AtualizarPessoaCommand.cs

--- src/API/Application/Commands/PessoaCommand/AtualizarPessoaCommand.cs
+++ src/API/Application/Commands/PessoaCommand/AtualizarPessoaCommand.cs
@@ -27,7 +27,7 @@ namespace API.Application.Commands.PessoaCommand
 
                 RuleFor(x => x.Nome)
                     .MinimumLength(2).WithMessage("O nome precisa ter pelo menos 2 caracteres")
-                    .MaximumLength(150).WithMessage("O nome precisa só pode ter no máximo 150");
+                    .MaximumLength(150).WithMessage("O nome pode ter no máximo 150 caracteres");
 
                 RuleFor(x => x.DataNascimento)
                     .GreaterThan(DateTime.MinValue)
