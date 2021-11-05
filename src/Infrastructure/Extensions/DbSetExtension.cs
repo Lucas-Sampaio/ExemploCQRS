b45e9a31d0b86f91551a7a8df3eaf57f86c8ce10
lucas.sampaio <lucas.sampaio@linx.com.br>
2021-11-05 00:04:35 -0300

    FET-002 criado extensão para adicionar props de navegação nas entidades para retono

---
+++ src/Infrastructure/Extensions/DbSetExtension.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace Infrastructure.Extensions
+{
+    public static class DbSetExtension
+    {
+        public static IQueryable<T> DynamicInclude<T>(this DbSet<T> dbSet, params string[] props) where T : class
+        {
+            var propriedades = typeof(T).GetProperties();
+            var query = dbSet.AsNoTracking();
+            for (int i = 0; i < props.Length; i++)
+            {
+                if (propriedades.Any(x => x.Name == props[i]))
+                    query = query.Include(props[i]);
+            }
+            return query;
+        }
+    }
+}
