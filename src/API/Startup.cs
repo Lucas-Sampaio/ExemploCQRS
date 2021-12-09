6328ccd429e5ca4d3e9f6ff7834687fe805e8602
lucas.sampaio <lucas.sampaio@linx.com.br>
2021-12-09 00:08:27 -0300

    FET-012 adicionado logs com serilog + elastic

--- src/API/Startup.cs
+++ src/API/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace API
 {
@@ -25,15 +26,15 @@ namespace API
             services.AddCustomHealthChecks(Configuration);
             services.AddAutoMapper(typeof(Startup));
             services.AddMediatR(typeof(Startup));
-          
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
             app.UseApiConfiguration(env);
             app.UseCustomHealthCheckConfiguration();
             app.UseSwaggerConfiguration();
+            SerilogConfig.ConfigureSerilog(Configuration, loggerFactory);
         }
     }
 }
